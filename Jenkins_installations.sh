0c164a741efdbba9f03607eb228c42f3cad699c8
Deepak Ram <154036627+Deepakram0929@users.noreply.github.com>
2025-06-19 12:32:57 +0530

    Initial changes

--- Jenkins_installations.sh
+++ Jenkins_installations.sh
@@ -1,4 +1,4 @@
-select 
+#select 
 # Amazon Machine Image (AMI)
 # Amazon Linux 2 AMI (HVM) - Kernel 5.10, SSD Volume Type
 
@@ -9,7 +9,7 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo \
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade
 # Add required dependencies for the jenkins package
-sudo yum install fontconfig java-21-openjdk
-sudo yum install jenkins
+sudo yum install fontconfig java-21-openjdk -y
+sudo yum install jenkins -y
 sudo systemctl daemon-reload
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
