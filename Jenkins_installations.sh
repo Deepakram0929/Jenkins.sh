ab2473d46b5328c70f60387f7b8f994a49891abb
Deepak Ram <154036627+Deepakram0929@users.noreply.github.com>
2025-06-19 12:21:20 +0530

    Initial changes

---
+++ Jenkins_installations.sh
@@ -0,0 +1,11 @@
+sudo yum install maven -y git
+sudo yum install java-1.8.0 -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
+# Add required dependencies for the jenkins package
+sudo yum install fontconfig java-21-openjdk
+sudo yum install jenkins
+sudo systemctl daemon-reload
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
